39aa90b7c73b79aee4edbb527458873cde6de9a4
derrokip4130 <derrickip34@gmail.com>
2024-12-20 14:59:59 +0300

    initialize flask extensions and register blueprints

---
+++ backend/app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from app.extensions import db, bcrypt, jwt, migrate
+from app.routes.auth_routes import auth_blueprint
+from app.routes.main_routes import main_blueprint
+from app.config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Initialize extensions
+    db.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+    migrate.init_app(app,db)
+
+    # Register blueprints
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(main_blueprint)
+
+    return app
